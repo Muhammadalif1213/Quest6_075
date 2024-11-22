3a20211c0c72d0f5dffe2b5d8e3ceb34c2285abc
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-11-22 12:30:59 +0700

    menambahkan parameter bertipe string dan modifier di fungsi DynamicSelectedTextField

--- app/src/main/java/com/example/project8/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/project8/ui/widget/DynamicSelectedField.kt
@@ -1,8 +1,15 @@
 package com.example.project8.ui.widget
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun DynamicSelectedTextField(){
+fun DynamicSelectedTextField(
+    selectedValue:String,
+    options:String,
+    label:String,
+    onValueChangedEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+){
 
 }

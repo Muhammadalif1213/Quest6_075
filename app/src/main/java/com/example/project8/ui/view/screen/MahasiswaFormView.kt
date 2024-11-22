f6d319119127aaa206a1c47a5dff44f498c97e8d
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-11-22 13:27:28 +0700

    membuat fungsi MahasiswaFormView dan parameternya

--- app/src/main/java/com/example/project8/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/project8/ui/view/screen/MahasiswaFormView.kt
@@ -1,2 +1,11 @@
 package com.example.project8.ui.view.screen
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MahasiswaFormView(
+    onSubmitedButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+){
+
+}

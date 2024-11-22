7947daf2cd7ad0369f5d229709d342dd8d7f32e7
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-11-22 15:28:14 +0700

    membuat fungsi RencanaStudyView dan membuat parameternya

--- app/src/main/java/com/example/project8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/project8/ui/view/screen/RencanaStudyView.kt
@@ -1,2 +1,11 @@
 package com.example.project8.ui.view.screen
 
+import com.example.project8.model.Mahasiswa
+
+fun RencanaStudyView(
+    mahasiswa : Mahasiswa,
+    onSubmitedButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+){
+
+}

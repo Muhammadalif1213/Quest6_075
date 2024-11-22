8b7d2e79ab37079fec13bd855c219cae9c19dc23
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-11-22 16:38:57 +0700

    membuat mahasiswa view model, menambahkan variabel privat, dan menambahkan fungsi saveDatamahasiswa

--- app/src/main/java/com/example/project8/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/project8/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -1,4 +1,31 @@
 package com.example.project8.ui.view.viewmodel
 
-class MahasiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.project8.model.Mahasiswa
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+
+class MahasiswaViewModel: ViewModel() {
+    //Request atau Event
+    private val _mahasiswaStateUI =
+        MutableStateFlow(Mahasiswa())
+
+    //respon
+    val mahasiswaUiState:
+            StateFlow<Mahasiswa> =
+        _mahasiswaStateUI.asStateFlow()
+
+    fun saveDataMahasiswa(ls: MutableList<String>) {
+        _mahasiswaStateUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nim = ls[0],
+                nama = ls[1],
+                email = ls[2]
+            )
+        }
+    }
+
 }

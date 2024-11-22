6e61cd55f733ccd6416268d0ae8028dc7b92a619
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-11-22 16:18:11 +0700

    membuat check box untuk menyetujui pernyataan, dan menambahkan 2 button untuk kembali dan lanjut

--- app/src/main/java/com/example/project8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/project8/ui/view/screen/RencanaStudyView.kt
@@ -14,6 +14,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
@@ -124,6 +127,33 @@ fun RencanaStudyView(
                         )
                         Text(data)
                     }
+                }
+            }
+            Spacer(modifier = Modifier.padding(8.dp))
+            HorizontalDivider()
+            Spacer(modifier = Modifier.padding(8.dp))
+            Text(text = "Klausul Persetujuan Mahasiswa", fontWeight = FontWeight.Bold)
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Checkbox(
+                    checked = checked,
+                    onCheckedChange = {checked = it},
+                    enabled = chosenDropdown.isNotBlank() && pilihanKelas.isNotBlank()
+                )
+                Text(
+                    text = "saya menyetujui setiap pernyataan yg ada tanpa ada paksaan dari pihak manapun",
+                    fontWeight = FontWeight.Light, fontSize = 10.sp
+                )
+            }
+            Spacer(modifier = Modifier.padding(8.dp))
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ){
+                Button(onClick = {onBackButtonClicked() }) {
+                    Text(text = "Kembali")
+                }
+                Button(onClick = {onSubmitedButtonClicked(listData)}, enabled = checked) {
+                    Text(text = "Lanjut")
                 }
             }
         }

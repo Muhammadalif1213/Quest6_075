bb8e92d53664421d6e8e6da061eeab79dfac26ac
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-11-22 13:31:05 +0700

    menambahkan variabel data mahasiswa di fungsi MahasiswaFormView

--- app/src/main/java/com/example/project8/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/project8/ui/view/screen/MahasiswaFormView.kt
@@ -1,11 +1,29 @@
 package com.example.project8.ui.view.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun MahasiswaFormView(
     onSubmitedButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit
 ){
+    var nama by remember {
+        mutableStateOf("")
+    }
 
+    var nim by remember {
+        mutableStateOf("")
+    }
+
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var listData: MutableList<String> = mutableListOf(nim,nama,email)
+
+    
 }
